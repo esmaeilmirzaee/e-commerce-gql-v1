42110984c4e5e65161a7b8843bd9d03064961291
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-11-09 22:42:35 +0330

    Refactor

--- src/apollo/Query.js
+++ src/apollo/Query.js
@@ -2,13 +2,13 @@ exports.Query = {
     getProduct: (_, { id }, { products }) => {
         return products.find((product) => product.id == id);
     },
-    getProducts: (_, args, { products }) => {
+    getProducts: (_, _, { products }) => {
         return products;
     },
     getCategory: (_, { id }, { categories }) => {
         return categories.find((category) => category.id == id);
     },
-    getCategories: (_, args, { categories }) => {
+    getCategories: (_, _, { categories }) => {
         return categories;
     },
 };
